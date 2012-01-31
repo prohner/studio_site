0ed9d02ed5fbe77cafca5fa03da7d2a2e0fccdcc
Preston Rohner <prestonrohner@me.com>
2012-01-31 09:29:35 -0800

    Done wtih static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def site_name
+    "Studio Site Support"
+  end
+  
+  def page_name(page_name)
+    if  page_name.empty?
+      site_name
+    else
+      site_name + " | " + page_name
+    end
+      
+  end
 end
