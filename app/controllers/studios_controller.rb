646c0147a2d1f26c44b486ec81997ae7b0d68203
Preston Rohner <prestonrohner@me.com>
2012-01-31 23:12:51 -0800

    Did signup form

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,10 +1,28 @@
 class StudiosController < ApplicationController
   def new
-    @title = "Sign Up"
+    @studio = Studio.new
+    @title = signup_form_name
   end
   
   def show
     @studio = Studio.find(params[:id])
     @title = @studio.name
   end
+  
+  def create
+    @studio = Studio.new(params[:studio])
+    if @studio.save
+      flash[:success] = "Welcome and thanks for signing up!"
+      redirect_to @studio
+    else
+      @title = signup_form_name
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def signup_form_name
+      "Sign Up"
+    end
 end
