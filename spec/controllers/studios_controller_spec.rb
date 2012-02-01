6da7ef35bf5eff90936c9047114322fadccea397
Preston Rohner <prestonrohner@me.com>
2012-02-01 08:11:04 -0800

    Basic index of users

--- spec/controllers/studios_controller_spec.rb
+++ spec/controllers/studios_controller_spec.rb
@@ -38,6 +38,42 @@ describe StudiosController do
     end
   end
   
+  describe "GET 'index'" do
+    describe "for non-signed-in studios" do
+      it "should deny access" do
+        get :index
+        response.should redirect_to(signin_path)
+        flash[:notice].should =~ /sign in/i
+      end
+    end
+    
+    describe "for signed-in studios" do
+      before(:each) do
+        @studio   = test_sign_in(Factory(:studio))
+        second    = Factory(:studio, :name => "Bill", :email => "lmno@pqrs.com")
+        third     = Factory(:studio, :name => "Tom",  :email => "asdf@pqrs.com")
+        @studios  = [@studio, second, third]
+      end
+      
+      it "should be successful" do
+        get :index
+        response.should be_success
+      end
+      
+      it "should have the right title" do
+        get :index
+        response.should have_selector("title", :content => "All Studios")
+      end
+      
+      it "should have an element for each studio" do
+        get :index
+        @studios.each do |studio|
+          response.should have_selector("li", :content => studio.name)
+        end
+      end
+    end
+  end
+  
   describe "GET 'edit'" do
     before(:each) do
       @studio = Factory(:studio)
