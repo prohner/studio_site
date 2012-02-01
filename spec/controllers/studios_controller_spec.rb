52e6e226b2b2341a4aae515d31f50a860bd6985a
Preston Rohner <prestonrohner@me.com>
2012-01-31 23:12:51 -0800

    Did signup form

--- spec/controllers/studios_controller_spec.rb
+++ spec/controllers/studios_controller_spec.rb
@@ -46,4 +46,49 @@ describe StudiosController do
     end
   end
 
+  describe "POST 'create'" do
+    describe "success" do
+      before(:each) do
+        @attr = { :name => "New User", :email => "abc@def.com", :password => "foobar1", :password_confirmation => "foobar1" }
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :studio => @attr
+        response.should redirect_to(studio_path(assigns(:studio)))
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :studio => @attr
+        end.should change(Studio, :count).by(1)
+      end
+      
+      it "should have a welcome message" do
+        post :create, :studio => @attr
+        flash[:success].should =~ /welcome/i
+      end
+    end
+    
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+      
+      it "should not create a user" do
+        lambda do
+          post :create, :studio => @attr
+        end.should_not change(Studio, :count)
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign Up")
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+  end
 end
