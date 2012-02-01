17a4735464477f743b5f4cc43002d11206a35695
Preston Rohner <prestonrohner@me.com>
2012-01-31 17:05:29 -0800

    User model with passwords and user show page

--- app/helpers/studios_helper.rb
+++ app/helpers/studios_helper.rb
@@ -1,2 +1,7 @@
 module StudiosHelper
+  def gravatar_for(studio, options = { :size => 50 })
+    gravatar_image_tag(studio.email.downcase, :alt => h(studio.name),
+                                              :class => 'gravatar',
+                                              :gravatar => options)
+  end
 end
