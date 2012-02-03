13e64443a91811ac2309846e984e82d266c86815
Preston Rohner <prestonrohner@me.com>
2012-02-02 23:30:03 -0800

    Ability to add a new Term

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,5 +1,15 @@
 class TermsController < ApplicationController
+  before_filter :authenticate, :only => [:create, :destroy]
+
   def create
+    term_group = TermGroup.find_by_id(params[:term][:term_group_id])
+    term = term_group.terms.build(params[:term])
+    if term.save
+      flash[:success] = "Term created"
+      redirect_to :controller => :studios, :action => :show, :id => term_group.style.studio.id, :style_id => term_group.style.id
+    else
+      render 'pages/home'
+    end
   end
 
   def destroy
