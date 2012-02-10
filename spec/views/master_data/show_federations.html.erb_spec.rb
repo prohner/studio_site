2b87c87aa0fef9c903ecae9e5ed59afb8bf982f4
Preston Rohner <prestonrohner@me.com>
2012-02-09 22:36:05 -0800

    A bit of testing

--- spec/views/master_data/show_federations.html.erb_spec.rb
+++ spec/views/master_data/show_federations.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe "master_data/show_federations.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/views/master_data/show_styles.html.erb_spec.rb
+++ spec/views/master_data/show_styles.html.erb_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe "master_data/show_styles.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+#  render_views
+#  
+#  before(:each) do
+#    @studio = Factory(:studio)
+#    @term_group = Factory(:term_group, :studio_id => @studio.id)
+#  end
+#  
+#  it "should be cool" do
+#    master_data_show_styles_path(:target_term_group_id => @term_group.id)
+#  end
 end
