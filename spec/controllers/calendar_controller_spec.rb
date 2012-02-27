5a69da2da3ecb8cf1568db26f8f50df24c1e8038
Preston Rohner <prestonrohner@me.com>
2012-02-26 20:32:05 -0800

    More testing cleanup

--- spec/controllers/calendar_controller_spec.rb
+++ spec/controllers/calendar_controller_spec.rb
@@ -20,7 +20,6 @@ describe CalendarController do
       response.should have_selector('div.calendar_entry')
     end
     
-    it "should show calendar events for only one studio at a time"
   end
 
 end
